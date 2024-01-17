0ec3956c75170b484b190a5f4b16d9983f59a8d2
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2024-01-17 10:44:43 -0300

    feat: adding first routes

---
+++ users/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def login(request):
+    return HttpResponse('Login')
+
+def register(request):
+    return HttpResponse('Register')
