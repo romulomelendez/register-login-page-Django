bf12156135e421b1cffd7f6445e0ad910aa3bf07
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2024-01-20 09:18:09 -0300

    feat: register page stylized

--- users/views.py
+++ users/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def login(request):
-    return render(request, 'login.html')
+    if request.method == 'GET':
+        return render(request, 'login.html')
 
 def register(request):
-    return render(request, 'register.html')
+    if request.method == 'GET':
+        return render(request, 'register.html')
